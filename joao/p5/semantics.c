f0aba7a5d07ab3e05d4631d94a3097d18fef8cc7
sjooaaoo <santoshpjoao2003@gmail.com>
2023-11-21 10:15:44 +0000

    ex1 - ficha 5

---
+++ joao/p5/semantics.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ast.h"
+#include "semantics.h"
+
+int semantic_errors = 0;
+
+struct symbol_list *symbol_table;
+
+void check_parameters(struct node *params) {
+    // struct node *param = getchild(params, 0);
+    // char *tkn = getchild(param, 1)->token;
+    // if(search_symbol(symbol_table, tkn) == NULL) {
+    //     insert_symbol(symbol_table, tkn, no_type, param);
+    // }
+    // else {
+    //     printf("Identifier %s already declared\n", tkn);
+    //     semantic_errors++;
+    // }
+
+    struct node_list* param = params->children->next;
+    char *tkn;
+    while (param) {
+        tkn = getchild(param->node, 1)->token;
+        if(search_symbol(symbol_table, tkn) == NULL) {
+            insert_symbol(symbol_table, tkn, no_type, param->node);
+        }
+        else {
+            printf("Identifier %s already declared\n", tkn);
+            semantic_errors++;
+        }
+
+        param = param->next;
+    }
+}
+
+void check_function(struct node *function) {
+    struct node *id = getchild(function, 0);
+    if(search_symbol(symbol_table, id->token) == NULL) {
+        insert_symbol(symbol_table, id->token, no_type, function);
+    } else {
+        printf("Identifier %s already declared\n", id->token);
+        semantic_errors++;
+    }
+    check_parameters(getchild(function, 1));
+    //check_expression(getchild(function, 2));
+}
+
+// semantic analysis begins here, with the AST root node
+int check_program(struct node *program) {
+    symbol_table = (struct symbol_list *) malloc(sizeof(struct symbol_list));
+    symbol_table->next = NULL;
+    struct node_list *child = program->children;
+    while((child = child->next) != NULL)
+        check_function(child->node);
+    return semantic_errors;
+}
+
+// insert a new symbol in the list, unless it is already there
+struct symbol_list *insert_symbol(struct symbol_list *table, char *identifier, enum type type, struct node *node) {
+    struct symbol_list *new = (struct symbol_list *) malloc(sizeof(struct symbol_list));
+    new->identifier = strdup(identifier);
+    new->type = type;
+    new->node = node;
+    new->next = NULL;
+    struct symbol_list *symbol = table;
+    while(symbol != NULL) {
+        if(symbol->next == NULL) {
+            symbol->next = new;    /* insert new symbol at the tail of the list */
+            break;
+        } else if(strcmp(symbol->next->identifier, identifier) == 0) {
+            free(new);
+            return NULL;           /* return NULL if symbol is already inserted */
+        }
+        symbol = symbol->next;
+    }
+    return new;
+}
+
+// look up a symbol by its identifier
+struct symbol_list *search_symbol(struct symbol_list *table, char *identifier) {
+    struct symbol_list *symbol;
+    for(symbol = table->next; symbol != NULL; symbol = symbol->next)
+        if(strcmp(symbol->identifier, identifier) == 0)
+            return symbol;
+    return NULL;
+}
+
+void show_symbol_table() {
+    struct symbol_list *symbol;
+    for(symbol = symbol_table->next; symbol != NULL; symbol = symbol->next)
+        printf("Symbol %s : %s\n", symbol->identifier, type_name(symbol->type));
+}
